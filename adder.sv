c75979b131627478d5792ef9bad5fbb8c5f95393
Max Golub <mgolub2@uw.edu>
2015-10-30 00:49:55 -0700

    32 bit adder version

--- adder.sv
+++ adder.sv
@@ -7,7 +7,7 @@ Author: Max Golub
 
 module adder(out, A, B, Cin);
 
-	parameter width = 8;
+	parameter width = 32;
 	parameter DELAY = 50;
  	output [width-1:0] out;
   	input [width-1:0] A, B;
@@ -29,17 +29,10 @@ module adder(out, A, B, Cin);
  	assign Gi1[0] = pg[0].G;
  	assign Pi2[0] = pg[0].P;
  	assign Gi2[0] = pg[0].G;
- 	//pass down bit one
- 	assign Pi1[1] = P[1];
- 	assign Gi1[1] = G[1];
- 	assign Pi2[1] = Pi1[1];
- 	assign Gi2[1] = Gi1[1];
- 	//two
- 	assign Pi2[2] = Pi1[2];
- 	assign Gi2[2] = Gi1[2];
- 	//3
- 	assign Pi2[3] = Pi1[3];
- 	assign Gi2[3] = Gi1[3];
+ 	assign Pi3[0] = pg[0].P;
+ 	assign Gi3[0] = pg[0].G;
+ 	assign Pi4[0] = pg[0].P;
+ 	assign Gi4[0] = pg[0].G;
 
 
 	generate 
@@ -47,12 +40,52 @@ module adder(out, A, B, Cin);
 			//wire P, G; 
 			//wire Pi1, Gi1;
 			//wire Pi2, Gi2;
-			if(i > 0) //generate the first set of modules, which always connect back to i-1 except for 0.
+			if(i > 0) begin//generate the first set of modules, which always connect back to i-1 except for 0.
+				//pass down bit one
+				if (i<2) begin
+				 	assign Pi1[i] = P[i];
+				 	assign Gi1[i] = G[i];
+				 	assign Pi2[i] = Pi1[i];
+				 	assign Gi2[i] = Gi1[i];
+				 	assign Pi3[i] = Pi1[i];
+				 	assign Gi3[i] = Gi1[i];
+				 	assign Pi4[i] = Pi1[i];
+				 	assign Gi4[i] = Gi1[i];
+				end
 				ks_sub  top_level(.P(P[i]), .G(G[i]), .P1(pg[i].P), .G1(pg[i].G), .P0(pg[i-1].P), .G0(pg[i-1].G));
-			if(i > 1)
+			end
+			if(i > 1) begin
+				if (i < 4) begin
+					//2-3
+					assign Pi2[i] = Pi1[i];
+				 	assign Gi2[i] = Gi1[i];
+				 	assign Pi3[i] = Pi1[i];
+				 	assign Gi3[i] = Gi1[i];
+				 	assign Pi4[i] = Pi1[i];
+				 	assign Gi4[i] = Gi1[i];
+				end
 				ks_sub midlevel_1(.P(Pi1[i]), .G(Gi1[i]), .P1(P[i]), .G1(G[i]), .P0(P[i-2]), .G0(G[i-2]));
-			if(i > 3) 
+			end
+			if(i > 3) begin
+				if (i < 8) begin
+					//4-7
+				 	assign Pi3[i] = Pi2[i];
+				 	assign Gi3[i] = Gi2[i];
+				 	assign Pi4[i] = Pi2[i];
+				 	assign Gi4[i] = Gi2[i];
+				end
 				ks_sub midlevel_2(.P(Pi2[i]), .G(Gi2[i]), .P1(Pi1[i]), .G1(Gi1[i]), .P0(Pi1[i-4]), .G0(Gi1[i-4]));
+			end
+			if(i > 7) begin
+				if(i < 16) begin
+					//8-15
+				 	assign Pi4[i] = Pi3[i];
+				 	assign Gi4[i] = Gi3[i]; 
+				end
+				ks_sub midlevel_3(.P(Pi3[i]), .G(Gi3[i]), .P1(Pi2[i]), .G1(Gi2[i]), .P0(Pi2[i-8]), .G0(Gi2[i-8]));
+			end
+			if(i > 15) 
+				ks_sub midlevel_4(.P(Pi4[i]), .G(Gi4[i]), .P1(Pi3[i]), .G1(Gi3[i]), .P0(Pi3[i-16]), .G0(Gi3[i-16]));
 		end
 	endgenerate
 
